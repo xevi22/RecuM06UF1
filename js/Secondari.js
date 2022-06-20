76feb56ced49d24c065e73843419086c864b52ad
xevi <xavier.pares@cirvianum.cat>
2022-06-20 18:45:27 +0200

    Fet

---
+++ js/Secondari.js
@@ -0,0 +1,17 @@
+addEventListener("load",()=>{
+    /*Afegeix el codi aquí per posar el valor inicial del control
+    * de data a dia d'avui*/
+})
+
+const saveCookies = ()=>{
+    /*Afegeix el codi aquí que faci el que es demana a la funció.
+    * codi ben estructurat amb nom de funcions significatius - res de codi
+    * espaghetti*/
+}
+
+const clearCookies=()=> {
+    /*Afegeix el codi aquí que faci el que es demana a la funció.
+    * codi ben estructurat amb nom de funcions significatius - res de codi
+    * espaghetti*/
+}
+
